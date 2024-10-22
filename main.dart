e3ec4bc9ca6cab69329193ef90607f59c5bd749f
dxdiag333 <dmmnpdclid12tf45@gmail.com>
2024-10-22 12:07:49 +0700

    Update main.dart
    telah mempelajari flutter codelab make app pretier

--- main.dart
+++ main.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart'; // paket tampilan ui
 import 'package:provider/provider.dart'; // paket interaksi
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MyApp(
+
+    )
+  );
 }
 
 // membuat class abstract dari class StatelessWidget untuk tujuan aplikasi
@@ -23,7 +27,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App', // informasi judul aplikasi
         theme: ThemeData( // informasi tema
           useMaterial3: true, // menggunakan useMaterial3
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange), // menggunakan warnaschema oren
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 0, 0)), // menggunakan warnaschema oren
         ),
         home: MyHomePage(), // list widget
       ),
@@ -44,14 +48,15 @@ class MyAppState extends ChangeNotifier {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
+    var appState = context.watch<MyAppState>(); /// menggunakan state myappstate
+    // di bawah ini adalah kode program untuk menyusun layout
+    var pair = appState.current; // menyimpan kata yang sedang aktif
 
     return Scaffold( // base layout
       body: Column(
         children: [
-          Text('hello world:'),
-          Text(pair.asLowerCase),
+          Text('hello test:'),
+          BigCards(pair: pair),
           ElevatedButton( // create button
             onPressed: () { // bila di tekan tombolnya maka akan exekusi function shadow dengan value print hello world
               appState.getNext();
@@ -59,6 +64,30 @@ class MyHomePage extends StatelessWidget {
             child: Text("klik"), // ini adalah teks dari button
           )
         ],
+      ),
+    );
+  }
+}
+
+class BigCards extends StatelessWidget {
+  const BigCards({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
